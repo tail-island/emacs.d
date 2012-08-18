3c2cc93260019f8127bc560754e1986d1ea31deb
OJIMA Ryoji <rojima1@gmail.com>
2012-08-18 11:25:07 +0700

    Reset fringe-mode to original.

--- init.el
+++ init.el
@@ -66,12 +66,12 @@
 (defun set-appearance ()
   (scroll-bar-mode 0)
   (tool-bar-mode 0)
-  (fringe-mode 0)
+  ;; (fringe-mode 0)
   (column-number-mode t)
   (when mac?
     (setq-default line-spacing 2))
   (when linux?
-    (setq-default line-spacing 2)
+    (setq-default line-spacing 1)
     (menu-bar-mode 0)))
 
 ;; 動作を設定します。
